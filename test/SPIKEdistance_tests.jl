ed9ca88d53177f4eb3c3b87cff505a4306f5e7a1
George Datseris <datseris.george@gmail.com>
2019-09-10 15:07:41 +0200

    distance analytic test

--- test/SPIKEdistance_tests.jl
+++ test/SPIKEdistance_tests.jl
@@ -3,13 +3,17 @@ using Test, SpikeSynchrony
 ####### ANALYTICAL TEST ########
 y1 = [1]
 y2 = [2]
-expected_times = [0, 1, 2, 3]
+ta = [0, 0, 1, 1, 2, 2, 3, 3]
 # I calculated S by hand for these trains:
-analytic_S = [0, 0, 5/9, 3/8, 3/8, 5/9, 0, 0]
+Sa = [0, 0, 5//9, 3//8, 3//8, 5//9, 0, 0]
 
 t, S = SPIKE_distance_profile(y1, y2; tf = 3)
-@test unique(t) == expected_times
-@test S ≈ analytic_S    atol = 1e-15
+@test t == ta
+@test S ≈ Sa    atol = 1e-15
+
+D_S_a = sum( 1//2 * (Sa[2:end] .+ Sa[1:end-1]) .* (ta[2:end] .- ta[1:end-1]))//(ta[end] - ta[1])
+D_S = SpikeSynchrony.trapezoid_integral(t, S)/(t[end] - t[1])
+@test D_S ≈ D_S_a    atol = 1e-15
 
 ####### Second Analytic test ########
 y1 = [2.0, 5.0, 8.0]
