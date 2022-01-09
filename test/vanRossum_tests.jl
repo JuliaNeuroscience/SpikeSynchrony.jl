2d000b8db1c78bcaa16e4173e456a2fcf321a36e
Russell Jarvis <russelljarvis@protonmail.com>
2022-01-09 19:58:25 +1100

    minor change to tests to declare scope of 'g' and suppress warnings (#8)

--- test/vanRossum_tests.jl
+++ test/vanRossum_tests.jl
@@ -12,7 +12,8 @@ end
 # Distance of two identical vectors but the second has one extra spike,
 # it is analytically known to be 1.0 (with our normalization)
 # irrespectively of the length of the vectors, for any τ.: eq (7)
-g = copy(f); push!(g, 11.0)
+global g = copy(f);
+push!(g, 11.0)
 for τ in 10 .* rand(10)
     @test vanRossum(f, g, τ) ≈ 1.0
     # @test vanRossum_fast(f, g, 1.0) ≈ 1.0
@@ -21,7 +22,7 @@ end
 # Shifting a spike from f to g produces a known result: eq. (8)
 for δt in (0.25, 0.5)
     for τ in 10 .* rand(10)
-        g = copy(f);
+        local g = copy(f);
         g[end] = f[end] + δt
         # divide by 2 due to our normalization
         @test vanRossum(f, g, τ)^2 / 2 ≈ 1.0 - exp(-δt/τ)
